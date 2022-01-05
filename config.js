85bcb4e90d039e3be9a5cf21f388d483c29c0a47
DDdestroyer10 <72734861+DDdestroyer10@users.noreply.github.com>
2022-01-05 15:56:54 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+//import { initializeApp } from "firebase/app";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDjpQO61qr18wtgCxXRgX6PiHFtnaB5Iw0",
+  authDomain: "story-app-60b0a.firebaseapp.com",
+  databaseURL: "https://story-app-60b0a-default-rtdb.firebaseio.com",
+  projectId: "story-app-60b0a",
+  storageBucket: "story-app-60b0a.appspot.com",
+  messagingSenderId: "794022039930",
+  appId: "1:794022039930:web:50319f4b9eb7f9b0fa06c3"
+};
+
+//const app = initializeApp(firebaseConfig);
